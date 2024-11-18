dc42c0e0645654734fe4d5820424662ecfddaabc
MyeongSeop Kim <yasu_ne@naver.com>
2024-11-18 16:30:07 +0900

    Create MemoryPool.cpp
    메모리 풀 관련 소스 추가

---
+++ ServerCore/MemoryPool.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "MemoryPool.h"
+
+MemoryPool::~MemoryPool()
+{
+    while (_queue.empty() == false)
+    {
+        MemoryHeader* header = _queue.front();
+        _queue.pop();
+        ::free(header);
+    }
+}
+
+void MemoryPool::Push(MemoryHeader* ptr)
+{
+    WRITE_LOCK;
+    ptr->allocSize = 0;
+
+    _queue.push(ptr);
+    _allocCount.fetch_sub(1);
+}
+
+MemoryHeader* MemoryPool::Pop()
+{
+    MemoryHeader* header = nullptr;
+
+    {
+        WRITE_LOCK;
+        if (_queue.empty() == false)
+        {
+            header = _queue.front();
+            _queue.pop();
+        }
+    }
+
+    void* test = ::malloc(_allocSize);
+    if (header == nullptr)
+    {
+        header = reinterpret_cast<MemoryHeader*>(::malloc(_allocSize));
+    }
+    else
+    {
+        ASSERT_CRASH(header->allocSize == 0);
+    }
+
+    _allocCount.fetch_add(1);
+    return header;
+}
