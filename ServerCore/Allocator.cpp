a3a6e8eac93f9287d0208f738d42f47b8aac8a4b
MyeongSeop Kim <yasu_ne@naver.com>
2024-11-18 16:34:20 +0900

    MemoryPool 관련 추가 소스 서밋

--- ServerCore/Allocator.cpp
+++ ServerCore/Allocator.cpp
@@ -13,7 +13,7 @@ void BaseAllocator::Release(void* ptr)
 
 void* StompAllocator::Alloc(int32 size)
 {
-	// PAGE_SIZE ¸¸Å­ ÇÒ´çÇÏ°í, °´Ã¼ÀÇ ¸Ş¸ğ¸® À§Ä¡¸¦ ³¡À¸·Î ¿Å°Ü ¿À¹öÇÃ·Î¿ì¸¦ ¹æÁöÇÔ.
+	// PAGE_SIZE ë§Œí¼ í• ë‹¹í•˜ê³ , ê°ì²´ì˜ ë©”ëª¨ë¦¬ ìœ„ì¹˜ë¥¼ ëìœ¼ë¡œ ì˜®ê²¨ ì˜¤ë²„í”Œë¡œìš°ë¥¼ ë°©ì§€í•¨.
 	const int64 pageCount = (size + PAGE_SIZE - 1) / PAGE_SIZE;
 	const int64 dataOffset = pageCount * PAGE_SIZE - size;
 	void* baseAddress = ::VirtualAlloc(NULL, pageCount * PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
@@ -26,4 +26,14 @@ void StompAllocator::Release(void* ptr)
 	const int64 address = reinterpret_cast<int64>(ptr);
 	const int64 baseAddress = address - (address % PAGE_SIZE);
 	::VirtualFree(reinterpret_cast<void*>(baseAddress), 0, MEM_RELEASE);
+}
+
+void* PoolAllocator::Alloc(int32 size)
+{
+	return GMemory->Allocate(size);
+}
+
+void PoolAllocator::Release(void* ptr)
+{
+	GMemory->Release(ptr);
 }
