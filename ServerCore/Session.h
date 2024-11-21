7b225c4dc17348a839ea6e26a696cd6e49de152d
MyeongSeop Kim <yasu_ne@naver.com>
2024-11-21 10:07:00 +0900

    세션 관련 기본 클래스 추가

---
+++ ServerCore/Session.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "IocpCore.h"
+#include "IocpEvent.h"
+#include "NetAddress.h"
+
+class Session : public IocpObject
+{
+public:
+    Session();
+    virtual ~Session();
+
+public:
+
+    void SetNetAddress(NetAddress netaddress) { _netAddress = netaddress; }
+    NetAddress GetAddress() { return _netAddress; }
+    SOCKET GetSocket() { return _socket; }
+
+public:
+
+    virtual HANDLE GetHandle() override;
+    virtual void Dispatch(class IocpEvent* iocpEvent, int32 numOfBytes = 0) override;
+
+public:
+    char            _recvBuffer[1000];
+private:
+    SOCKET          _socket = INVALID_SOCKET;
+    NetAddress      _netAddress = {};
+    Atomic<bool>    _connected = false;
+
+};
+
